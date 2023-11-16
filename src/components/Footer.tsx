ce503734975fc2fd1b45c41300f4509e0f7a3cca
Rahul Sulegaokar <rsulegaokar@gmail.com>
2023-11-16 18:46:01 +0530

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,8 @@
 
 const navigation = {
-  solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
+  services: [
+    { name: 'Indian Services', href: '#' },
+    { name: 'Global Services', href: '#' },
   ],
   support: [
     { name: 'Pricing', href: '#' },
@@ -13,11 +11,8 @@ const navigation = {
     { name: 'API Status', href: '#' },
   ],
   company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
+    { name: 'About Us', href: '#' },
+    { name: 'Contact Us', href: '#' }
   ],
   legal: [
     { name: 'Claim', href: '#' },
@@ -60,49 +55,23 @@ const navigation = {
         </svg>
       ),
     },
-    {
-      name: 'GitHub',
-      href: '#',
-      icon: (props:any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'YouTube',
-      href: '#',
-      icon: (props:any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
   ],
 }
 
-export default function Example() {
+export default function Footer() {
   return (
-    <footer className="bg-gray-900" aria-labelledby="footer-heading">
+    <footer className="bg-gray-900 bg-[url('/images/bg/testimonial.svg')] bg-cover" aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-32">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div className="xl:grid xl:grid-cols-2 xl:gap-8">
           <div className="grid grid-cols-2 gap-8 xl:col-span-2">
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
-                <h3 className="text-sm font-semibold leading-6 text-white">Solutions</h3>
+                <h3 className="text-sm font-semibold leading-6 text-white">Services</h3>
                 <ul role="list" className="mt-6 space-y-4">
-                  {navigation.solutions.map((item) => (
+                  {navigation.services.map((item) => (
                     <li key={item.name}>
                       <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
                         {item.name}
@@ -111,20 +80,6 @@ export default function Example() {
                   ))}
                 </ul>
               </div>
-              <div className="mt-10 md:mt-0">
-                <h3 className="text-sm font-semibold leading-6 text-white">Support</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.support.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
                 <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
                 <ul role="list" className="mt-6 space-y-4">
@@ -137,7 +92,33 @@ export default function Example() {
                   ))}
                 </ul>
               </div>
-              <div className="mt-10 md:mt-0">
+              {/* <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-white">Support</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.support.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div> */}
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              {/* <div>
+                <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.company.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div> */}
+              {/* <div className="mt-10 md:mt-0">
                 <h3 className="text-sm font-semibold leading-6 text-white">Legal</h3>
                 <ul role="list" className="mt-6 space-y-4">
                   {navigation.legal.map((item) => (
@@ -148,10 +129,10 @@ export default function Example() {
                     </li>
                   ))}
                 </ul>
-              </div>
+              </div> */}
             </div>
           </div>
-          <div className="mt-10 xl:mt-0">
+          {/* <div className="mt-10 xl:mt-0">
             <h3 className="text-sm font-semibold leading-6 text-white">Subscribe to our newsletter</h3>
             <p className="mt-2 text-sm leading-6 text-gray-300">
               The latest news, articles, and resources, sent to your inbox weekly.
@@ -178,7 +159,7 @@ export default function Example() {
                 </button>
               </div>
             </form>
-          </div>
+          </div> */}
         </div>
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
           <div className="flex space-x-6 md:order-2">
--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -14,9 +14,9 @@ export default function Testimonials() {
             excellence.
           </p>
         </div>
-        <div className="gird  my-8">
+        <div className="gird my-8">
           <Marquee className="h-full">
-            <div className="border h-[480px] w-full bg-[url('/images/bg/testimonial2.svg')] p-12 md:border border-gray-700 md:py-16   max-w-lg min-w-lg">
+            <div className="border h-[550px] w-full bg-[url('/images/bg/testimonial2.svg')] p-12 md:border border-gray-700 md:py-16   max-w-lg min-w-lg">
               <blockquote className="mt-6 md:flex md:flex-grow md:flex-col">
                 <div className="relative text-md font-medium text-white md:flex-grow">
                   <svg
@@ -30,11 +30,11 @@ export default function Testimonials() {
                     I had the pleasure of working with CA Khushboo Aggarwal and
                     I am continually impressed by her exceptional skills and
                     dedication to work. She possesses a deep knowledge of
-                    financial matters and a keen eye for detail. They
+                    financial matters and a keen eye for detail. <br/><br/>They
                     consistently deliver results, whether it&apos;s in managing
                     complex financial data or providing valuable insights for
                     strategic decisions.
-                    <br />
+                    <br /><br/>
                     Moreover, Khushboo is a highly reliable and proactive
                     professional. Her expertise and professionalism have been
                     invaluable
@@ -62,7 +62,7 @@ export default function Testimonials() {
                 </footer>
               </blockquote>
             </div>
-            <div className="border h-[480px] w-full bg-[url('/images/bg/testimonial2.svg')] p-12 md:border border-gray-700 md:py-16   max-w-lg min-w-lg">
+            <div className="border h-[550px] w-full bg-[url('/images/bg/testimonial2.svg')] p-12 md:border border-gray-700 md:py-16   max-w-lg min-w-lg">
               <blockquote className="mt-6 md:flex md:flex-grow md:flex-col">
                 <div className="relative text-md font-medium text-white md:flex-grow">
                   <svg
@@ -74,7 +74,7 @@ export default function Testimonials() {
                   </svg>
                   <p className="relative">
                     Had a very pleasant experience with their service and they
-                    are really friendly, very professional and knowledgeable.
+                    are really friendly, very professional and knowledgeable.<br/><br/>
                     They had solutions to all my accounting blunders and saved a
                     ton of my money. Even if you just consult them they will
                     give you very valuable guidance. Keep up the good work 👍
@@ -99,7 +99,7 @@ export default function Testimonials() {
                 </footer>
               </blockquote>
             </div>
-            <div className="border h-[480px] w-full bg-[url('/images/bg/testimonial2.svg')] p-12 md:border border-gray-700 md:py-16   max-w-lg min-w-lg">
+            <div className="border h-[550px] w-full bg-[url('/images/bg/testimonial2.svg')] p-12 md:border border-gray-700 md:py-16   max-w-lg min-w-lg">
               <blockquote className="mt-6 md:flex md:flex-grow md:flex-col">
                 <div className="relative text-md font-medium text-white md:flex-grow">
                   <svg
@@ -110,10 +110,7 @@ export default function Testimonials() {
                     <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                   </svg>
                   <p className="relative">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nemo expedita voluptas culpa sapiente alias molestiae.
-                    Numquam corrupti in laborum sed rerum et corporis. Nemo
-                    expedita voluptas culpa sapiente alias molestiae.
+                  My work is a bit complex as I work for the Government and before her, I tried everything from doing it myself to hiring an accountant, but I couldn&apos;t get things straight. Since I started working with her, I am not concerned about accounting, taxation, or legal. She handles it all for me.<br/><br/> She and her team are always available for me even if I call them late at night. That&apos;s the dedication she and her team show in their work. Not only this, their knowledge and confidence makes me free of all my worries.
                   </p>
                 </div>
                 <footer className="mt-8">
@@ -127,10 +124,10 @@ export default function Testimonials() {
                     </div>
                     <div className="ml-4">
                       <div className="text-base font-medium text-white">
-                        Joseph Rodriguez
+                        Filmy Chacha
                       </div>
                       <div className="text-base font-medium text-gray-400">
-                        CEO, Workcation
+                        CEO
                       </div>
                     </div>
                   </div>
