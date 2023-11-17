e9312e56977acf189dbc9badfdf21c6e742f76bf
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-11-17 16:39:17 +0530

    done

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -5,13 +5,13 @@ export default function Stats({ data }: any) {
         <div className="mx-auto max-w-2xl lg:max-w-none">
           <div className="text-left">
             <h2 className="text-xl  font-semibold tracking-tight text-white ">
-              Trusted by creators worldwide
+              Trusted by businesses worldwide
             </h2>
           </div>
           <dl className="mt-8 flex flex-col md:flex-row mx-auto overflow-hidden rounded-2xl text-center ">
             {data.features?.map((stat: any) => (
               <div key={stat.id} className="flex flex-col p-8">
-                <img src="/headphone.svg" className="w-8 h-8 mx-auto" />
+                {/* <img src="/headphone.svg" className="w-8 h-8 mx-auto" /> */}
                 <dt className="text-md mt-4 font-semibold leading-6 text-gray-300">
                   {stat.text}
                 </dt>
